d79893770045621fe5719dd0ce670595e5ec4c1e
chiyu_lee <chiyu.lee3@gmail.com>
2023-08-07 23:38:16 +0200

    add function to analyse and plot psd

--- create_curated_dataset.py
+++ create_curated_dataset.py
@@ -1,21 +1,13 @@
-
 from open_ephys.analysis import Session
+from psd_analysis import calculate_psd
+from neurodsp.utils import create_times
 import os, fnmatch
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from fooof import FOOOF
-
-# Initialize FOOOF object
-def fooof_analysis():
-    fm = FOOOF()
-
 # Define frequency range across which to model the spectrum
-    freq_range = [3, 40]
-
 # Model the power spectrum with FOOOF, and print out a report
     #fm.report(freqs, spectrum, freq_range)
-
 def main(thisDir, analysis_methods):
 
     session = Session(thisDir)
@@ -23,30 +15,45 @@ def main(thisDir, analysis_methods):
     print(session.recordnodes)
     #recordnode = session.recordnodes[0]
     recording = session.recordnodes[0].recordings[0]
-    data = recording.continuous[0].get_samples(start_sample_index=0, end_sample_index=10000)
-    print(data)
-    plt.close('all')
-    fig = plt.figure(figsize=(6,6))
-    ax = fig.add_subplot(111)
-    ax.axis('equal')
-    ax.plot(data[:,0], 'ro', label='test data', zorder=1)
-    svg_plot = "tracking_trace.svg"
-    thisPlot_svg = os.path.join(thisDir, svg_plot)
-    jpg_plot = "tracking_trace.jpg"
-    thisPlot_jpg = os.path.join(thisDir, jpg_plot)
-    if analysis_methods.get("Debug_mode") == True:
-        print("no picture is saved in debug mode")
+    fs=recording.info["continuous"][0]['sample_rate']
+    if analysis_methods.get("Analye_entire_recording")==True:
+        data_of_interest=len(recording.continuous[0].sample_numbers)
     else:
-        fig.savefig(thisPlot_svg)
-        fig.savefig(thisPlot_jpg)
-    #print(session.recordnodes[0].recordings[0])
-    return data,fig
+        data_of_interest=int(fs)*3
+    data = recording.continuous[0].get_samples(start_sample_index=0, end_sample_index=data_of_interest)  
+    times = create_times(data.shape[0]/fs, fs)
+    fig_out=analysis_methods.get("Fig_dir")
+    for i in range(32):
+        sig=data[:,i]
+        calculate_psd(sig,fs,times,i,fig_out)
+
+    
+    # print(data)
+    # plt.close('all')
+    # fig = plt.figure(figsize=(6,6))
+    # ax = fig.add_subplot(111)
+    # ax.axis('equal')
+    # ax.plot(data[:,0], 'ro', label='test data', zorder=1)
+    # svg_plot = "tracking_trace.svg"
+    # thisPlot_svg = os.path.join(thisDir, svg_plot)
+    # jpg_plot = "tracking_trace.jpg"
+    # thisPlot_jpg = os.path.join(thisDir, jpg_plot)
+
+    # if analysis_methods.get("Debug_mode") == True:
+    #     print("no picture is saved in debug mode")
+    # else:
+    #     fig.savefig(thisPlot_svg)
+    #     fig.savefig(thisPlot_jpg)
+    # #print(session.recordnodes[0].recordings[0])
+    # return data,fig
 
 if __name__ == "__main__":
     thisDir = "C:/Users/neuroLaptop/Documents/Open Ephys/2023-06-06_21-51-30/"
     analysis_methods = {
         "Overwrite_curated_dataset": True,
         "Reanalyse_data": True,
+        "Fig_dir":"Z:/DATA/experiment_openEphys/GN00001",
+        "Analye_entire_recording":True,
         "Plot_trace": False,
         "Debug_mode": True,
     }
---
+++ psd_analysis.py
@@ -0,0 +1,32 @@
+# Import utilities for loading and plotting data
+# Import spectral variance functions
+from neurodsp.spectral import compute_spectral_hist, compute_scv, compute_scv_rs
+# Import function to compute power spectra
+from neurodsp.spectral import compute_spectrum, rotate_powerlaw
+from neurodsp.plts.time_series import plot_time_series
+from neurodsp.plts.spectral import (plot_spectral_hist, plot_scv,
+                                    plot_scv_rs_lines, plot_scv_rs_matrix,plot_power_spectra)
+from neurodsp.plts.utils import check_ax, save_figure
+from fooof import FOOOF
+
+# Initialize FOOOF object
+def fooof_analysis():
+    fm = FOOOF()
+    freq_range = [3, 40]
+def calculate_psd(sig,fs,times,num,fig_dir):
+        #plot_time_series(times, sig)
+        # Mean of spectrogram (Welch)
+        freq_mean, psd_mean = compute_spectrum(sig, fs, method='welch', avg_type='mean', nperseg=fs*2)
+
+        # Median of spectrogram ("median Welch")
+        freq_med, psd_med = compute_spectrum(sig, fs, method='welch', avg_type='median', nperseg=fs*2)
+
+        # Median filtered spectrum
+        freq_mf, psd_mf = compute_spectrum(sig, fs, method='medfilt')
+        
+        file_name=f"power_spectra_channel{num}.png"
+        plot_power_spectra([freq_mean[:200], freq_med[:200], freq_mf[100:10000]],
+                   [psd_mean[:200], psd_med[:200], psd_mf[100:10000]],
+                   ['Welch', 'Median Welch', 'Median Filter FFT'])
+
+        save_figure(file_name,file_path=fig_dir,close=True)
