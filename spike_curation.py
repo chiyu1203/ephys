ebea24602cb74e3439c8e7263049b1a500acfe93
chiyu_lee <chiyu.lee3@gmail.com>
2025-05-21 20:47:25 +0200

    add automated labeling into analysis pipeline. Reorganised the extension calculation. Turn sparse to False when creating sorting analyzer

--- spike_curation.py
+++ spike_curation.py
@@ -17,6 +17,7 @@ import pandas as pd
 import spikeinterface.widgets as sw
 import matplotlib as mpl
 from matplotlib import cm
+from sklearn.metrics import confusion_matrix, balanced_accuracy_score
 
 warnings.simplefilter("ignore")
 n_cpus = os.cpu_count()
@@ -44,6 +45,20 @@ class MplColorHelper:
 colormap_name = "coolwarm"
 COL = MplColorHelper(colormap_name, 0, 8)
 
+def calculate_moving_avg(label_df, confidence_label, window_size):
+
+    label_df[f'{confidence_label}_decile'] = pd.cut(label_df[confidence_label], 10, labels=False, duplicates='drop')
+    # Group by decile and calculate the proportion of correct labels (agreement)
+    p_label_grouped = label_df.groupby(f'{confidence_label}_decile')['model_x_human_agreement'].mean()
+    # Convert decile to range 0-1
+    p_label_grouped.index = p_label_grouped.index / 10
+    # Sort the DataFrame by confidence scores
+    label_df_sorted = label_df.sort_values(by=confidence_label)
+
+    p_label_moving_avg = label_df_sorted['model_x_human_agreement'].rolling(window=window_size).mean()
+
+    return label_df_sorted[confidence_label], p_label_moving_avg
+
 def spike_overview(
     oe_folder,
     this_sorter,
@@ -69,10 +84,12 @@ def spike_overview(
 
     # spost.align_sorting(sorting_analyzer, drift_ptps)
     # si.get_template_extremum_channel_peak_shift
-    ax = sw.plot_unit_templates(sorting_analyzer, backend="matplotlib")
-    fig_name = f"preview_unit_template.png"
-    fig_dir = oe_folder / fig_name
-    ax.figure.savefig(fig_dir)
+    plot_fig=False
+    if plot_fig:
+        ax = sw.plot_unit_templates(sorting_analyzer, backend="matplotlib")
+        fig_name = f"preview_unit_template.png"
+        fig_dir = oe_folder / fig_name
+        ax.figure.savefig(fig_dir)
     spike_time_list = []
     spike_amp_list = []
     cluster_id_list = []
@@ -118,33 +135,33 @@ def spike_overview(
             unit_loc[this_unit][0] ** 2 + unit_loc[this_unit][1] ** 2
         )
         unit_loc_list.append(this_unit_loc)
-
-        plt.figure()
-        ax = plt.gca()
-        ax.scatter(x=spike_times, y=spike_loc_df["distance"].values, c=COL.get_rgb(i))
-        ax.set_xlim([0, recording_for_analysis.get_total_duration()])
-        ax.set_ylim([250, 350])
-        fig_name = f"spike_location_unit{this_unit}.svg"
-        fig_dir = oe_folder / fig_name
-        ax.figure.savefig(fig_dir)
-        # plot drift map based on brainbox's plotting function
-        ax_drift, x_lim, y_lim = driftmap_color(
-            clusters_depths=this_unit_loc,
-            spikes_times=spike_times,
-            spikes_amps=spike_amp[0][this_unit],
-            spikes_depths=spike_loc_df["distance"].values,
-            spikes_clusters=this_cluster_id,
-            ax=None,
-            axesoff=False,
-            return_lims=True,
-        )
-        # ax_drift.set_xlim([0, recording_for_analysis.get_total_duration()])
-        # ax_drift.set_ylim([250, 350])
-        ax_drift.set_xlim(x_lim)
-        ax_drift.set_ylim(y_lim)
-        fig_name = f"driftmap_unit{this_unit}.svg"
-        fig_dir = oe_folder / fig_name
-        ax_drift.figure.savefig(fig_dir)
+        if plot_fig:
+            plt.figure()
+            ax = plt.gca()
+            ax.scatter(x=spike_times, y=spike_loc_df["distance"].values, c=COL.get_rgb(i))
+            ax.set_xlim([0, recording_for_analysis.get_total_duration()])
+            ax.set_ylim([250, 350])
+            fig_name = f"spike_location_unit{this_unit}.svg"
+            fig_dir = oe_folder / fig_name
+            ax.figure.savefig(fig_dir)
+            # plot drift map based on brainbox's plotting function
+            ax_drift, x_lim, y_lim = driftmap_color(
+                clusters_depths=this_unit_loc,
+                spikes_times=spike_times,
+                spikes_amps=spike_amp[0][this_unit],
+                spikes_depths=spike_loc_df["distance"].values,
+                spikes_clusters=this_cluster_id,
+                ax=None,
+                axesoff=False,
+                return_lims=True,
+            )
+            # ax_drift.set_xlim([0, recording_for_analysis.get_total_duration()])
+            # ax_drift.set_ylim([250, 350])
+            ax_drift.set_xlim(x_lim)
+            ax_drift.set_ylim(y_lim)
+            fig_name = f"driftmap_unit{this_unit}.svg"
+            fig_dir = oe_folder / fig_name
+            ax_drift.figure.savefig(fig_dir)
         i += 1
 
     ##try to use this driftmap_color or driftmap
@@ -155,48 +172,73 @@ def spike_overview(
         np.concatenate(spike_loc_list),
     )
 def calculate_analyzer_extension(sorting_analyzer):
+    # ext = sorting_analyzer.get_extension("spike_amplitudes")
+    # ext_data = ext.get_data()
+    ##use this to get documentation ext.get_data?
+
+    ''' 
+    Question: PCA why 5 instead of 3; templates why operator average std and not ms before and after; spike_location why center of_mass
+    print(sorting_analyzer.get_default_extension_params('random_spikes'))
+    {'method': 'uniform', 'max_spikes_per_unit': 500, 'margin_size': None, 'seed': None}
+    print(sorting_analyzer.get_default_extension_params('noise_levels'))
+    {}
+    print(sorting_analyzer.get_default_extension_params('correlograms'))
+    {'window_ms': 50.0, 'bin_ms': 1.0, 'method': 'auto'}
+    print(sorting_analyzer.get_default_extension_params('isi_histograms'))
+    {'window_ms': 50.0, 'bin_ms': 1.0, 'method': 'auto'}
+    print(sorting_analyzer.get_default_extension_params('templates'))
+    {'ms_before': 1.0, 'ms_after': 2.0, 'operators': None}
+    print(sorting_analyzer.get_default_extension_params('waveforms'))
+    {'ms_before': 1.0, 'ms_after': 2.0, 'dtype': None}
+    print(sorting_analyzer.get_default_extension_params('principal_components'))
+    {'n_components': 5, 'mode': 'by_channel_local', 'whiten': True, 'dtype': 'float32'}
+    print(sorting_analyzer.get_default_extension_params('unit_locations'))
+    {'method': 'monopolar_triangulation'}
+    print(sorting_analyzer.get_default_extension_params('spike_amplitudes'))
+    {'peak_sign': 'neg'}
+    print(sorting_analyzer.get_default_extension_params('spike_locations'))
+    {'ms_before': 0.5, 'ms_after': 0.5, 'spike_retriver_kwargs': None, 'method': 'center_of_mass', 'method_kwargs': {}}
+    print(sorting_analyzer.get_default_extension_params('template_metrics'))
+    {'metric_names': None, 'peak_sign': 'neg', 'upsampling_factor': 10, 'sparsity': None, 'metric_params': None, 'metrics_kwargs': None, 'include_multi_channel_metrics': False, 'delete_existing_metrics': False}
+    print(sorting_analyzer.get_default_extension_params('template_similarity'))
+    {'method': 'cosine', 'max_lag_ms': 0, 'support': 'union'}
+    print(sorting_analyzer.get_default_extension_params('quality_metrics'))
+    {'metric_names': None, 'metric_params': None, 'qm_params': None, 'peak_sign': None, 'seed': None, 'skip_pc_metrics': False, 'delete_existing_metrics': False, 'metrics_to_compute': None}
+
+    '''
     sorting_analyzer.compute(
-        ["random_spikes", "isi_histograms", "correlograms", "noise_levels"]
-    )
-    # sorting_analyzer.compute(["waveforms", "principal_components", "templates"])
-    sorting_analyzer.compute("waveforms")
-    compute_dict = {
-        "principal_components": {"n_components": 3, "mode": "by_channel_local"},
-        "templates": {"operators": ["average"]},
-    }
-    sorting_analyzer.compute(compute_dict)
-    compute_dict = {
+        {
+        "noise_levels":{},
+        "random_spikes":{'max_spikes_per_unit':1000},
+        "correlograms":{'bin_ms':0.5},
+        "isi_histograms":{},
+        "waveforms":{},
+        "principal_components":{},
+        "templates":{},
         "unit_locations": {"method": "monopolar_triangulation"},
-        "spike_amplitudes": {"peak_sign": "neg"},
-    }
-    sorting_analyzer.compute(compute_dict)
-    #isolate analysis of spike location here as it has more parameters
-    # sorting_analyzer.compute(input="spike_locations")
-    # drift_ptps, drift_stds, drift_mads = sqm.compute_drift_metrics(sorting_analyzer=sorting_analyzer,peak_sign="neg")
-    # sorting_analyzer.compute(
-    # input="spike_locations",
-    # ms_before=0.5,
-    # ms_after=0.5,
-    # spike_retriever_kwargs=dict(
-    #     channel_from_template=True,
-    #     radius_um=50,
-    #     peak_sign="neg"
-    # ),
-    # method="center_of_mass")
-    #
-    sorting_analyzer.compute(
-        [
-            "spike_locations",
-            "template_metrics",
-            "template_similarity",
-            "amplitude_scalings",
-            "quality_metrics",
-        ]
+        "spike_amplitudes": {},
+        "spike_locations":{"method":"monopolar_triangulation"},
+        "template_metrics":{"include_multi_channel_metrics":True},
+        "template_similarity":{},
+        "quality_metrics":{}
+         }
     )
+    # sorting_analyzer.compute("waveforms")
+    # compute_dict = {
+    #     "principal_components": {"n_components": 3},
+    #     "templates": {"operators": ["average"]},#needs to check if average or ms_before or ms after is better according to Guided Spikeinterface
+    # }
+    # drift_ptps, drift_stds, drift_mads = sqm.compute_drift_metrics(sorting_analyzer=sorting_analyzer,peak_sign="neg")
+    #
+    # sorting_analyzer.compute(
+    #     [
+    #         #"amplitude_scalings",
+    #     ]
 
 
 def si2phy(thisDir, json_file):
     oe_folder = Path(thisDir)
+    c_drive=Path(r"C:\Users\einat\Documents\Open Ephys\2025-05-12_19-17-47")
     if isinstance(json_file, dict):
         analysis_methods = json_file
     else:
@@ -211,7 +253,7 @@ def si2phy(thisDir, json_file):
     analyser_folder_name = "analyser" + sorter_suffix
     phy_folder_name = "phy" + sorter_suffix
     report_folder_name = "report" + sorter_suffix
-    remove_excess_spikes = 1
+    remove_excess_spikes = True
 
     if (
         analysis_methods.get("load_analyser_from_disc") == True
@@ -236,6 +278,7 @@ def si2phy(thisDir, json_file):
         )
         # sorting_duduplicated = scur.remove_duplicated_spikes(sorting_spikes)
         # sorting_spikes = sorting_duduplicated
+        print(scur.find_redundant_units(sorting_spikes))
         unit_labels = sorting_spikes.get_property("quality")
         recording_saved = get_preprocessed_recording(oe_folder,analysis_methods)
         analysis_methods.update({"load_existing_motion_info": True})
@@ -245,10 +288,11 @@ def si2phy(thisDir, json_file):
             recording_for_analysis=si.aggregate_channels(recording_corrected_dict)
         else:
             recording_for_analysis=recording_corrected_dict[0]
+
         sorting_analyzer = si.create_sorting_analyzer(
             sorting=sorting_spikes,
             recording=recording_for_analysis,
-            sparse=True,  # default
+            sparse=False,  # default
             format="binary_folder",
             folder=oe_folder / analyser_folder_name,
             overwrite=True,  # default  # default
@@ -281,14 +325,16 @@ def si2phy(thisDir, json_file):
         else:
             recording_for_analysis=recording_corrected_dict[0]
         if remove_excess_spikes:
-            sorting_wout_excess_spikes = scur.remove_excess_spikes(
+            sorting_spikes=scur.remove_duplicated_spikes(sorting_spikes,censored_period_ms=0.3,method="keep_first_iterative")
+            sorting_spikes = scur.remove_excess_spikes(
                 sorting_spikes, recording_for_analysis
             )
-            sorting_spikes = sorting_wout_excess_spikes
+            print(scur.find_redundant_units(sorting_spikes))
+            #sorting_spikes= scur.remove_redundant_units(sorting_spikes,align=False) this is used in the Guided Spikeinterface Hands-on but not sure if I should use it 
         sorting_analyzer = si.create_sorting_analyzer(
             sorting=sorting_spikes,
             recording=recording_for_analysis,
-            sparse=True,  # default
+            sparse=False,  # default
             format="memory",  # default
         )
         calculate_analyzer_extension(sorting_analyzer)
@@ -301,35 +347,108 @@ def si2phy(thisDir, json_file):
                 output_folder=oe_folder / phy_folder_name,
                 compute_amplitudes=True,
                 compute_pc_features=True,
-                copy_binary=False,
+                copy_binary=True,
                 remove_if_exists=True,
             )
-        # else:
-        #     isi_viol_thresh = 0.5
-        #     amp_cutoff_thresh = 0.1
-        #     our_query = f"amplitude_cutoff < {amp_cutoff_thresh} & isi_violations_ratio < {isi_viol_thresh}"
-        #     print(our_query)
-        #     keep_units = sqm.query(our_query)
-        #     keep_unit_ids = keep_units.index.values
-        sqm.compute_quality_metrics(sorting_analyzer,metric_names=["isolation_distance","d_prime"])
-        sw.plot_quality_metrics(sorting_analyzer, include_metrics=["amplitude_cutoff", "presence_ratio", "isi_violations_ratio", "snr","isolation_distance","d_prime"])
+            print(scur.get_potential_auto_merge(sorting_analyzer))
+            print(f"postprocessing is done. Export the files to phy to do manual curation")
+        else:
+            print("try out some automatic curation")
+            quality_metrics=sqm.compute_quality_metrics(sorting_analyzer)
+            isi_viol_thresh = 0.5
+            amp_cutoff_thresh = 0.1
+            rp_thresh=0.2
+            sd_thresh=1.5
+            snr_thresh=1.1
+            firing_rate_thresh=1.0
+            #curation_rule = f"amplitude_cutoff < {amp_cutoff_thresh} & isi_violations_ratio < {isi_viol_thresh}" #the one used in spikeinterface notebook
+            curation_rule = f"firing_rate > {firing_rate_thresh} & snr > {snr_thresh} & rp_contamination < {rp_thresh} & sd_ratio < {sd_thresh}" #the one used in Guided Spikeinterface Hands-on
+            good_metrics=quality_metrics.query(curation_rule)
+            curated_unit_ids=list(good_metrics.index)
+            print(curated_unit_ids)
+        ### the following information should be calculated in calculate_analyzer_extension already
+            # print(sqm.get_quality_metric_list())
+            # sqm.compute_quality_metrics(sorting_analyzer,metric_names=["isolation_distance","d_prime","snr","sd_ratio"])
+
+
+        
     else:
         return print(
             f"{sorting_folder_name} is not found. Noting can be done here without some putative spikes..."
         )
 
+    noise_neuron_labels = scur.auto_label_units(sorting_analyzer = sorting_analyzer,repo_id ="SpikeInterface/UnitRefine_noise_neural_classifier",trust_model=True) #or ['numpy.dtype']
+    # Apply the noise/not-noise model
+    noise_units = noise_neuron_labels[noise_neuron_labels['prediction']=='noise']
+    noise_units.to_csv(oe_folder / analyser_folder_name/'predicted_noise_units.csv')
+    print(noise_units)
+    analyzer_neural = sorting_analyzer.remove_units(noise_units.index)
+    # Apply the sua/mua model
+    sua_mua_labels = scur.auto_label_units(
+        sorting_analyzer=analyzer_neural,
+        repo_id="SpikeInterface/UnitRefine_sua_mua_classifier",
+        trust_model=True,
+    )
+    all_labels = pd.concat([sua_mua_labels, noise_units]).sort_index()
+    all_labels.to_csv(oe_folder / analyser_folder_name/'predicted_sua_mua.csv')
+
+    if (analysis_methods.get("load_curated_spikes") == True
+        and (oe_folder / phy_folder_name).is_dir()):
+        #evalute the performance between automated labels and manual labels
+        model,model_info=scur.load_model(repo_id="SpikeInterface/UnitRefine_noise_neural_classifier",trusted=['numpy.dtype'])
+        human_labels = sorting_analyzer.sorting.get_property('quality')
+        label_conversion = model_info['label_conversion']
+        predictions = noise_neuron_labels['prediction']
+        conf_matrix = confusion_matrix(human_labels, predictions)
+        # Calculate balanced accuracy for the confusion matrix
+        balanced_accuracy = balanced_accuracy_score(human_labels, predictions)
+
+        plt.imshow(conf_matrix)
+        for (index, value) in np.ndenumerate(conf_matrix):
+            plt.annotate( str(value), xy=index, color="white", fontsize="15")
+        plt.xlabel('Predicted Label')
+        plt.ylabel('Human Label')
+        plt.xticks(ticks = [0, 1], labels = list(label_conversion.values()))
+        plt.yticks(ticks = [0, 1], labels = list(label_conversion.values()))
+        plt.title('Predicted vs Human Label')
+        plt.suptitle(f"Balanced Accuracy: {balanced_accuracy}")
+        plt.show()
+
+        confidences = noise_neuron_labels['probability']
+
+        # Make dataframe of human label, model label, and confidence
+        label_df = pd.DataFrame(data = {
+            'human_label': human_labels,
+            'decoder_label': predictions,
+            'confidence': confidences},
+            index = sorting_analyzer.sorting.get_unit_ids())
+
+        # Calculate the proportion of agreed labels by confidence decile
+        label_df['model_x_human_agreement'] = label_df['human_label'] == label_df['decoder_label']
+
+        p_agreement_sorted, p_agreement_moving_avg = calculate_moving_avg(label_df, 'confidence', 3)
+
+        # Plot the moving average of agreement
+        plt.figure(figsize=(6, 6))
+        plt.plot(p_agreement_sorted, p_agreement_moving_avg, label = 'Moving Average')
+        plt.axhline(y=1/len(np.unique(predictions)), color='black', linestyle='--', label='Chance')
+        plt.xlabel('Confidence'); #plt.xlim(0.5, 1)
+        plt.ylabel('Proportion Agreement with Human Label'); plt.ylim(0, 1)
+        plt.title('Agreement vs Confidence (Moving Average)')
+        plt.legend(); plt.grid(True); plt.show()
+
+ 
+    sw.plot_quality_metrics(sorting_analyzer, include_metrics=["isi_violations_ratio","snr","rp_contamination","firing_rate","sd_ratio"])
     ax = sw.plot_unit_templates(sorting_analyzer, backend="matplotlib")
     fig_name = f"preview_unit_template.png"
     fig_dir = oe_folder / fig_name
     ax.figure.savefig(fig_dir)
 
-    # drift_ptps, drift_stds, drift_mads = sqm.compute_drift_metrics(
-    #     sorting_analyzer=sorting_analyzer
-    # )
     if analysis_methods.get("export_report") == True:
         sep.export_report(
             sorting_analyzer, output_folder=oe_folder / report_folder_name
         )
+    return sorting_analyzer
 
 
 if __name__ == "__main__":
@@ -338,7 +457,11 @@ if __name__ == "__main__":
     # thisDir = r"Z:\DATA\experiment_trackball_Optomotor\Zball\GN23015\240201\coherence\session1\2024-02-01_15-25-25"
     #thisDir = r"Z:\DATA\experiment_openEphys\P-series-32channels\2025-02-26_17-00-43"del rec_for_sorting[2]
     #thisDir = r"Z:\DATA\experiment_openEphys\H-series-128channels\2025-03-23_21-33-38"
-    thisDir = r"Z:\DATA\experiment_openEphys\H8-stacked-128channels\2025-03-23_21-33-38"
+    #thisDir = r"Z:\DATA\experiment_openEphys\H8-stacked-128channels\2025-03-23_21-33-38"
+    #thisDir = r"D:\Open Ephys\2025-05-10_21-23-07"
+    #thisDir = r"D:\Open Ephys\2025-05-12_19-17-47"
+    #thisDir = r"D:\Open Ephys\2025-05-17_19-17-15"
+    thisDir = r"D:\Open Ephys\2025-05-17_19-33-07"
     #thisDir = r"D:\Open Ephys\2025-03-05_13-45-15"
     #thisDir = r"D:\Open Ephys\2025-04-03_19-13-57"
     #thisDir = r"D:\Open Ephys\2025-04-09_19-33-08"
@@ -347,6 +470,6 @@ if __name__ == "__main__":
     json_file = "./analysis_methods_dictionary.json"
     ##Time the function
     tic = time.perf_counter()
-    si2phy(thisDir, json_file)
+    _=si2phy(thisDir, json_file)
     toc = time.perf_counter()
     print(f"it takes {toc-tic:0.4f} seconds to run the main function")
